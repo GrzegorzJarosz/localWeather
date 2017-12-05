0e630bcb90955571ccac1547c902794166b282dc
Grzegorz Jarosz <grzegorz82jarosz@gmail.com>
2017-12-05 23:36:50 +0100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+
+var coords={lat:'', lon:''};
+var serviceUrl;
+/*------------------------------------------------------------------------*/
+var calendar= function(timeClassContainer,dateClassContainer){
+
+	var setDate=function(){
+		var now=new Date();
+		var DD = now.getDate();
+		var MM = now.getMonth()+1;
+		var YY = now.getFullYear();
+		var hh = now.getHours();
+		var mm = now.getMinutes();
+		var ss = now.getSeconds();
+		if (DD<10){DD = "0"+DD;}
+		if (MM<10){MM = "0"+MM;}
+		if (hh<10){hh = "0"+hh;}
+		if (mm<10){mm = "0"+mm};
+		if (ss<10){ss = "0"+ss};
+		var readyTime=hh+':'+mm+':'+ss;
+		var readyDate=DD+'.'+MM+'.'+YY;
+		var dateCont=document.querySelector('.'+dateClassContainer).innerHTML=readyDate;
+		var timeCont=document.querySelector('.'+timeClassContainer).innerHTML=readyTime;
+	}
+setInterval(setDate,1000);
+};
+
+/*------------------------------------------------------------------------*/
+var getCoordinates=function(){
+	navigator.geolocation.getCurrentPosition(function(pos){
+		var crd= pos.coords;
+		coords.lat=crd.latitude;
+		coords.lon=crd.longitude;
+		getData();
+	});
+};
+/*------------------------------------------------------------------------*/
+var getData=function(){
+	serviceUrl=`http://api.wunderground.com/api/899e303598994b68/conditions/lang:PL/q/${coords.lat},${coords.lon}.json`;
+
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		 if (this.readyState == 4 && this.status == 200) {
+			  var data = JSON.parse(this.responseText);
+			  displayer(data);
+		   }
+	};
+	xmlhttp.open("get", serviceUrl, true);
+	xmlhttp.send();
+};
+
+/*------------------------------------------------------------------------*/
+var displayer = function(data){
+	
+	var src=document.querySelectorAll('.kreciol');
+	src.forEach(function(a){a.classList.remove("kreciol");})
+
+	document.querySelector('.cond-img img').src=data.current_observation.icon_url;
+	document.querySelector('.cond-descr').innerHTML=data.current_observation.weather;
+	document.querySelector('.val-temp').innerHTML=data.current_observation.temp_c;
+	document.querySelector('.val-humidity').innerHTML=data.current_observation.relative_humidity;
+	document.querySelector('.val-press').innerHTML=data.current_observation.pressure_mb;
+	document.querySelector('.val-wind').innerHTML=data.current_observation.wind_mph;
+
+	var city=data.current_observation.observation_location.city.split(',',1);
+	var country=data.current_observation.observation_location.country
+	var location= city +', '+ country;
+	document.querySelector('.city').innerHTML=location;
+};
+calendar('time','date');
+getCoordinates();

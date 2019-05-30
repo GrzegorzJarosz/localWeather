b66c6cf110451b26a56ceaed8025eb60fac2bc48
Grzegorz Jarosz <grzegorz82jarosz@gmail.com>
2019-05-30 19:37:19 +0200

    old api and logo removed

--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,80 @@
-
-var coords = {lat:'', lon:''};
-var serviceUrl;
+let coords = {
+	lat: '',
+	lon: ''
+};
+let serviceUrl;
 /*------------------------------------------------------------------------*/
-var calendar = function(timeClassContainer,dateClassContainer){
+let calendar = function (timeClassContainer, dateClassContainer) {
 
-	var setDate = function(){
-		var now = new Date();
-		var DD = now.getDate();
-		var MM = now.getMonth()+1;
-		var YY = now.getFullYear();
-		var hh = now.getHours();
-		var mm = now.getMinutes();
-		var ss = now.getSeconds();
-		if (DD<10){DD = "0"+DD;}
-		if (MM<10){MM = "0"+MM;}
-		if (hh<10){hh = "0"+hh;}
-		if (mm<10){mm = "0"+mm};
-		if (ss<10){ss = "0"+ss};
-		var readyTime = hh+':'+mm+':'+ss;
-		var readyDate = DD+'.'+MM+'.'+YY;
-		var dateCont = document.querySelector('.'+dateClassContainer).innerHTML=readyDate;
-		var timeCont = document.querySelector('.'+timeClassContainer).innerHTML=readyTime;
+	let setDate = function () {
+		let now = new Date();
+		let DD = now.getDate();
+		let MM = now.getMonth() + 1;
+		let YY = now.getFullYear();
+		let hh = now.getHours();
+		let mm = now.getMinutes();
+		let ss = now.getSeconds();
+		if (DD < 10) {
+			DD = "0" + DD;
+		}
+		if (MM < 10) {
+			MM = "0" + MM;
+		}
+		if (hh < 10) {
+			hh = "0" + hh;
+		}
+		if (mm < 10) {
+			mm = "0" + mm
+		};
+		if (ss < 10) {
+			ss = "0" + ss
+		};
+		let readyTime = hh + ':' + mm + ':' + ss;
+		let readyDate = DD + '.' + MM + '.' + YY;
+		let dateCont = document.querySelector('.' + dateClassContainer).innerHTML = readyDate;
+		let timeCont = document.querySelector('.' + timeClassContainer).innerHTML = readyTime;
 	}
-setInterval(setDate,1000);
+	setInterval(setDate, 1000);
 };
 
 /*------------------------------------------------------------------------*/
-var getCoordinates = function(){
-	navigator.geolocation.getCurrentPosition(function(pos){
-		var crd = pos.coords;
+let getCoordinates = function () {
+	navigator.geolocation.getCurrentPosition(function (pos) {
+		let crd = pos.coords;
 		coords.lat = crd.latitude;
 		coords.lon = crd.longitude;
 		getData();
 	});
 };
 /*------------------------------------------------------------------------*/
-var getData = function(){
-	serviceUrl =`https://api.wunderground.com/api/899e303598994b68/conditions/lang:PL/q/${coords.lat},${coords.lon}.json`;
+let getData = function () {
 
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function() {
-		 if (this.readyState == 4 && this.status == 200) {
-			  var data = JSON.parse(this.responseText);
-			  displayer(data);
-		   }
+	const urlBase = 'https://api.weatherbit.io/v2.0/current';
+
+	serviceUrl = `urlBase`;
+
+
+	let xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			let data = JSON.parse(this.responseText);
+			displayer(data);
+		}
 	};
 	xmlhttp.open("get", serviceUrl, true);
 	xmlhttp.send();
 };
 
 /*------------------------------------------------------------------------*/
-var displayer = function(data){
+let displayer = function (data) {
 
-	var src = document.querySelectorAll('.kreciol');
-	src.forEach(function(a){a.classList.remove("kreciol");})
+	let src = document.querySelectorAll('.kreciol');
+	src.forEach(function (a) {
+		a.classList.remove("kreciol");
+	})
 
 	//mph to mps convert
-	var toMpsFactor = 0.44704;
+	let toMpsFactor = 0.44704;
 
 	document.querySelector('.cond-img img').src = data.current_observation.icon_url;
 	document.querySelector('.cond-descr').innerHTML = data.current_observation.weather;
@@ -65,10 +83,10 @@ var displayer = function(data){
 	document.querySelector('.val-press').innerHTML = data.current_observation.pressure_mb;
 	document.querySelector('.val-wind').innerHTML = Math.round(data.current_observation.wind_mph * toMpsFactor);
 
-	var city = data.current_observation.observation_location.city.split(',',1);
-	var country = data.current_observation.observation_location.country
-	var location = city +', '+ country;
+	let city = data.current_observation.observation_location.city.split(',', 1);
+	let country = data.current_observation.observation_location.country
+	let location = city + ', ' + country;
 	document.querySelector('.city').innerHTML = location;
 };
-calendar('time','date');
+calendar('time', 'date');
 getCoordinates();
